4953b5ca74f9ef05d19f1b62ede62d8911170b05
NikOverflow <nikoverflow007@gmail.com>
2024-02-22 16:08:17 +0100

    feat: add discord rich presence.
    Signed-off-by: NikOverflow <nikoverflow007@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # discord-rich-presence
+This is the Discord Rich Presence for SecSchool.
+
+## Contributing
+Please follow our [Style Guide](https://github.com/SecSchool/styleguide) when contributing to this project.
---
+++ main.py
@@ -0,0 +1,42 @@
+import time
+from pypresence import Presence
+
+connected = False
+wait = 10
+
+def connect():
+    global connected
+    global wait
+    try:
+        rpc = Presence(client_id="1193490043612971030")
+        print("Connecting...")
+        rpc.connect()
+        print("Connected.")
+        connected = True
+        wait = 10
+        return rpc
+    except Exception:
+        print("Could not connect!")
+        if wait < 30:
+            wait += 5
+        return None
+
+rpc = connect()
+try:
+    while True:
+        try:
+            if rpc is None:
+                rpc = connect()
+            rpc.update(details="Join SecSchool today!", large_image="secschool", large_text="SecSchool", buttons=[{"label": "Website", "url": "https://secschool.net"}, {"label": "Discord", "url": "https://discord.gg/2bWxKHn8Yd"}])
+        except Exception:
+            if connected:
+                print("Connection lost!")
+                connected = False
+                rpc = None
+            print(f"Trying to reconnect in {wait} seconds.")
+            pass
+        time.sleep(wait)
+except KeyboardInterrupt:
+    if rpc is not None:
+        rpc.close()
+    pass
